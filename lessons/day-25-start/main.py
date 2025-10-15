facd02fb4006313896cae9b4201e31a819fe65ff
Deniz <ayhandenialtas@icloud.com>
2025-10-15 11:01:30 -0400

    more pandas

--- lessons/day-25-start/main.py
+++ lessons/day-25-start/main.py
@@ -21,25 +21,67 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# database = pandas.read_csv("weather_data.csv")
+#
+# data_dict = data.to_dict()
+#
+# temp_list = data["temp"].to_list()
+# def calculate_average():
+#     sum_of_list = 0
+#
+#     for i in temp_list:
+#         sum_of_list += i
+#
+#
+#     average = sum_of_list / len(temp_list)
+#     print(average)
+#
+# calculate_average()
+#
+#
+# average = sum(temp_list) / len(temp_list)
+# print(average)
 
-data_dict = data.to_dict()
+# print(data["temp"].mean())
+# print(data["temp"].max())
 
-temp_list = data["temp"].to_list()
+# print(database[database.day == "Monday"])
+# print(database[database.temp == database.temp.max()])
+#
+
+# monday = database[database.day == "Monday"]
+# print(monday)
+#
+# celcius = monday["temp"]
+#
+# fahrenheit = (celcius * 1.8) + 32
+#
+# print(fahrenheit)
+
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 
-def calculate_average():
-    sum_of_list = 0
 
-    for i in temp_list:
-        sum_of_list += i
+blacks = data[data["Primary Fur Color"] == "Black"]
+grays = data[data["Primary Fur Color"] == "Gray"]
+reds = data[data["Primary Fur Color"] == "Cinnamon"]
 
+blacks_len = len(blacks)
+grays_len = len(grays)
+reds_len = len(reds)
 
-    average = sum_of_list / len(temp_list)
-    print(average)
+number_of_colors = {
+    "colors": ["blacks", "grays", "reds"],
+    "amount": [blacks_len, grays_len, reds_len]
+}
 
-calculate_average()
-
-
-average = sum(temp_list) / len(temp_list)
-print(average)
+squirrel_data = pandas.DataFrame(number_of_colors)
+squirrel_data.to_csv("squirrel_colors.csv")
---
+++ projects/us_states_game/main.py
@@ -0,0 +1,48 @@
+import turtle
+from turtle import Turtle
+import pandas
+
+ALIGNMENT = "center"
+FONT = ("Courier", 8, "normal")
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+writer_turtle = Turtle()
+writer_turtle.penup()
+writer_turtle.hideturtle()
+
+def write_state(nameofthestate,x,y):
+    """writer turtle writes the state name on x,y cordinate"""
+    writer_turtle.goto(x,y)
+    writer_turtle.write(f"{nameofthestate}", align=ALIGNMENT, font=FONT)
+
+states_df = pandas.read_csv("50_states.csv")
+
+
+list_of_states = states_df["state"].tolist()
+correct_guesses = []
+
+
+while len(correct_guesses) < 50:
+    answer_state = screen.textinput(title= f"{len(correct_guesses)}/50 States Correct", prompt= "What's another state's name?").title()
+    print(states_df[states_df["state"] == answer_state]["x"])
+
+    if answer_state == "Exit":
+        place_holder_all_states = list_of_states
+        missing = [state for state in place_holder_all_states if state not in correct_guesses]
+
+        df = pandas.DataFrame(missing)
+        df.to_csv('states_to_learn.csv')
+
+        break
+
+    if answer_state in list_of_states:
+        correct_guesses.append(answer_state)
+        goto_x = int(states_df[states_df["state"] == answer_state]["x"])
+        goto_y = int(states_df[states_df["state"] == answer_state]["y"])
+        write_state(answer_state,goto_x,goto_y)
+        print("awesome")
