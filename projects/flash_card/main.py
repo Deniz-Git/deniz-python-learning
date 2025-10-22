e1131ca14d88ab57e5c6c99f7a1d0d5937df761b
Ayhan Deniz ALTAS <ayhandenizaltas@EzginoMacBook-Pro.local>
2025-10-22 18:20:54 -0400

    working on it

--- projects/flash_card/main.py
+++ projects/flash_card/main.py
@@ -2,22 +2,34 @@ from tkinter import *
 import random
 import pyperclip
 import pandas as pd
-
 BACKGROUND_COLOR = "#B1DDC6"
 
 
 # ---------------------------- CARDS LIST ------------------------------- #
 
-df = pd.read_csv("/home/deniz/deniz-python-learning/projects/flash_card/data/french_words.csv")
+df = pd.read_csv("/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/data/french_words.csv")
 dictionary = df.to_dict(orient='records')
 
+current_card = {}
+
+
+
 def new_word_fun():
-    randomly_picked_word = random.choice(dictionary)['French']
-    canvas.itemconfig(word, text=randomly_picked_word)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card =  random.choice(dictionary)
+    canvas.itemconfig(language, text="French", fill="black")
+    canvas.itemconfig(word, text=current_card['French'], fill="black")
+    canvas.itemconfig(background_img, image=image_cardfront)
+    flip_timer = window.after(3000, func=flip)
+
+def flip():
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(word, text=current_card['English'], fill="white")
+    canvas.itemconfig(background_img, image=image_cardback)
 
 
-
-
+ 
 
 # ---------------------------- UI ------------------------------- #
 window = Tk()
@@ -27,25 +39,25 @@ window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
 # Canvas
 
 canvas = Canvas(width=800, height=526)
-image_cardfront = PhotoImage(file="/home/deniz/deniz-python-learning/projects/flash_card/images/card_front.png")
-canvas.create_image(400,263,image=image_cardfront)
-language = canvas.create_text(400,150,text="Title", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400,263,text="", font=("Ariel", 60, "bold"))
+image_cardfront = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/card_front.png")
+background_img = canvas.create_image(400,263,image=image_cardfront)
+language = canvas.create_text(400,150,text="Title", font=("Ariel", 40, "italic"),fill="black")
+word = canvas.create_text(400,263,text="", font=("Ariel", 60, "bold"),fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0,row=0, columnspan=2)
+image_cardback = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/card_back.png")
 
 
 
 # Buttons
 
-correct_button_img = PhotoImage(file="/home/deniz/deniz-python-learning/projects/flash_card/images/right.png")
+correct_button_img = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/right.png")
 correct_button = Button(image=correct_button_img,highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=new_word_fun)
 correct_button.grid(column=0,row=1)
 
-wrong_button_img = PhotoImage(file="/home/deniz/deniz-python-learning/projects/flash_card/images/wrong.png")
+wrong_button_img = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/wrong.png")
 wrong_button = Button(image=wrong_button_img, highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=new_word_fun)
 wrong_button.grid(column=1,row=1)
-
 # Labels
 
 # what_language_label = Label(text="French", font=("Ariel", 40, "italic"),bg="white")
@@ -54,10 +66,13 @@ wrong_button.grid(column=1,row=1)
 # word_label = Label(text="Word", font=("Ariel", 60, "bold"), bg="white")
 # word_label.place(x=270,y=250)
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
 
 
+
+flip_timer = window.after(3000, func=flip)
 new_word_fun()
 window.mainloop() 
 
