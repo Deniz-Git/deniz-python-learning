306310c1291a0de07e5ebf09410e131bc45d8ee7
Deniz <ayhandenizaltas@icloud.com>
2025-10-23 11:12:51 -0400

    flashcard app completed

--- projects/flash_card/main.py
+++ projects/flash_card/main.py
@@ -6,27 +6,51 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 # ---------------------------- CARDS LIST ------------------------------- #
+try:
+    df = pd.read_csv("~/deniz-python-learning/projects/flash_card/data/words_to_learn.csv")
+    dictionary = df.to_dict(orient='records')
+except FileNotFoundError:
+    df = pd.read_csv("~/deniz-python-learning/projects/flash_card/data/french_words.csv")
+    dictionary = df.to_dict(orient='records')
 
-df = pd.read_csv("/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/data/french_words.csv")
-dictionary = df.to_dict(orient='records')
+
+
+
+
 
 current_card = {}
 
-
-
-def new_word_fun():
+def get_new_word():
     global current_card, flip_timer
-    window.after_cancel(flip_timer)
     current_card =  random.choice(dictionary)
+    window.after_cancel(flip_timer)
     canvas.itemconfig(language, text="French", fill="black")
     canvas.itemconfig(word, text=current_card['French'], fill="black")
     canvas.itemconfig(background_img, image=image_cardfront)
     flip_timer = window.after(3000, func=flip)
 
+def check_button():
+    global current_card
+    dictionary.remove(current_card)
+    get_new_word()
+    to_be_saved = pd.DataFrame(dictionary)
+    to_be_saved.to_csv("~/deniz-python-learning/projects/flash_card/data/words_to_learn.csv", index=False)
+    
+
+def equis_button():
+    global current_card, flip_timer
+    get_new_word()
+    to_be_saved = pd.DataFrame(dictionary)
+    to_be_saved.to_csv("~/deniz-python-learning/projects/flash_card/data/words_to_learn.csv", index=False)
+    
+
+
+
 def flip():
     canvas.itemconfig(language, text="English", fill="white")
     canvas.itemconfig(word, text=current_card['English'], fill="white")
     canvas.itemconfig(background_img, image=image_cardback)
+
 
 
  
@@ -39,24 +63,24 @@ window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
 # Canvas
 
 canvas = Canvas(width=800, height=526)
-image_cardfront = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/card_front.png")
+image_cardfront = PhotoImage(file="~/deniz-python-learning/projects/flash_card/images/card_front.png")
 background_img = canvas.create_image(400,263,image=image_cardfront)
 language = canvas.create_text(400,150,text="Title", font=("Ariel", 40, "italic"),fill="black")
 word = canvas.create_text(400,263,text="", font=("Ariel", 60, "bold"),fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0,row=0, columnspan=2)
-image_cardback = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/card_back.png")
+image_cardback = PhotoImage(file="~/deniz-python-learning/projects/flash_card/images/card_back.png")
 
 
 
 # Buttons
 
-correct_button_img = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/right.png")
-correct_button = Button(image=correct_button_img,highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=new_word_fun)
+correct_button_img = PhotoImage(file="~/deniz-python-learning/projects/flash_card/images/right.png")
+correct_button = Button(image=correct_button_img,highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=check_button)
 correct_button.grid(column=0,row=1)
 
-wrong_button_img = PhotoImage(file="/Users/ayhandenizaltas/deniz-python-learning/projects/flash_card/images/wrong.png")
-wrong_button = Button(image=wrong_button_img, highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=new_word_fun)
+wrong_button_img = PhotoImage(file="~/deniz-python-learning/projects/flash_card/images/wrong.png")
+wrong_button = Button(image=wrong_button_img, highlightthickness=0, padx=0, pady=0,bg=BACKGROUND_COLOR, command=equis_button)
 wrong_button.grid(column=1,row=1)
 # Labels
 
@@ -73,6 +97,7 @@ wrong_button.grid(column=1,row=1)
 
 
 flip_timer = window.after(3000, func=flip)
-new_word_fun()
+get_new_word()
+print(current_card)
 window.mainloop() 
 
